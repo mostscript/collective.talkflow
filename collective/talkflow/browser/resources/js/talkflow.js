64bfa632a517c2bb8ef426883e4d0d10f1b7410b
Sean Upton <sdupton@gmail.com>
2017-03-02 11:18:26 -0700

    Question add form customizations, via javascript

--- collective/talkflow/browser/resources/js/talkflow.js
+++ collective/talkflow/browser/resources/js/talkflow.js
@@ -3,7 +3,20 @@
 
 (function ($) {
   $(document).ready(function () {
+    var addView = '++add++collective.talkflow.discussion';
     console.log('Talkflow ready');
+    if (window.location.pathname.indexOf(addView) !== -1) {
+      if (window.location.search.indexOf('addquestion') !== -1) {
+        console.log('Add a question');
+        $('#form-widgets-question-0').prop( "checked", true );
+        $('h1.documentFirstHeading').text('Add a Question to Discuss');
+        $('#formfield-form-widgets-IBasic-title label')
+          .append(
+            $('<span>(short version of question)</span>')
+          );
+        $('#formfield-form-widgets-question').hide();
+      }
+    }
   });
 }(jQuery));
 
